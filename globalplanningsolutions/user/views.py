ce0d45d9b9fcca9249930fd896a887f3b557b62b
luke13200 <30780920+luke13200@users.noreply.github.com>
2022-11-03 09:07:20 -0400

    created index page view in user app

--- globalplanningsolutions/user/views.py
+++ globalplanningsolutions/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_view(request):
+    """
+    URL: /
+    this view is for the static landing page.
+    """
+    return render(request, 'base.html', None);
